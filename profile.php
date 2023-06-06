06b54838feb66e51d055ca6713634aeb899e0b96
MuhammadArifullah <arifullah0602@gmail.com>
2023-06-06 21:43:57 +0800

    commit pertama: menambahkan semua fail hasil praktikum web 2 1 2 3 3

---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Arif.jpg" alt="Muhammad Arifullah" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010032</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Arifullah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Amuntai, 20 Juni 2002</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0838 3117 1403</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">arifullah0602@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.facebook.com/atif.fullah?mibextid=ZbWKwL" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://instagram.com/ayeeffyu?" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/icons8-twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/medsos/icons8-linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
